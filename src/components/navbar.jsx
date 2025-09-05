77024bbbc0a2f2ad3acdbdf63163f3b30f1652b4
mr-sardor <rozmatovsardor67@gmail.com>
2025-09-05 21:36:30 +0500

    input section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import searchIcon from '../assets/icons/search-icon.svg'
 
 
 const Navbar = () => {
@@ -9,7 +10,9 @@ const Navbar = () => {
             <div className='flex items-center gap-[190px]'>
                 <h1 className='font-bold text-2xl leading-6 tracking-[3%]'>Exclusive</h1>
                 <ul className='flex w-[367px] h-[24px] gap-[48px] mt-[7px] '>
-                    <li className='font-poppins font-normal text-base leading-6 tracking-[0%] text-center relative  after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[1.5px] after:bg-black after:-translate-x-1/2 after:transition-all after:duration-300 hover:after:w-full [&.active]:after:w-full active'>
+                    <li className='font-poppins font-normal text-base leading-6 tracking-[0%] text-center relative 
+                     after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[1.5px] after:bg-black after:-translate-x-1/2 
+                     after:transition-all after:duration-300 hover:after:w-full [&.active]:after:w-full active'>
                         <Link className='font-poppins font-[Poppins] no-underline text-black leading-6'>Home</Link>
                     </li>
                     <li className='font-poppins font-normal text-base leading-6 tracking-[0%] text-center relative  after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[1.5px] after:bg-black after:-translate-x-1/2 after:transition-all after:duration-300 hover:after:w-full [&.active]:after:w-full'>
@@ -23,8 +26,9 @@ const Navbar = () => {
                     </li>
                 </ul>
 
-                <div>
-                    <input/>
+                <div className='flex justify-between items-center bg-neutral-100 w-[243px] h-[38px] gap-[34px] rounded pl-6 pr-3 py-[7px]'>
+                   <input className='bg-transparent text-xs w-[90%] border-[none] outline-none' placeholder='What are you looking for?' />
+                   <img style={{cursor:"pointer"}} src={searchIcon} alt="serach-icon" />
                 </div>
             </div>
         </div>
