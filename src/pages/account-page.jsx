4fc18175581c5b84d0ec6a9f2845be69301d2b02
mr-sardor <rozmatovsardor67@gmail.com>
2025-09-25 13:47:38 +0500

    my account

--- src/pages/account-page.jsx
+++ src/pages/account-page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Account = () => {
   return (
@@ -13,7 +14,23 @@ const Account = () => {
           My Account
         </p>
       </div>
-      <p className='font-normal text-sm font-poppins text-[#000] cursor-pointer'>Welcome! <span className='text-[#DB4444]'>Md Rimel</span></p>
+      <p className='font-normal text-sm font-poppins text-[#000] cursor-pointer'>Welcome!
+         <span className='text-[#DB4444]'>Md Rimel</span></p>
+      </div>
+
+      <div className='w-[200px] h-[287px] mb-[343px] '>
+        <p className='font-medium text-base leading-6 tracking-[0%] font-poppins'>Manage My Account</p>
+        <div className='w-[165px] h-[88px] ml-[35px] mt-4'>
+            <Link className='font-normal text-base leading-6 tracking-[0%] font-poppins text-[#DB4444]'>My Profile</Link><br />
+            <Link className='font-normal text-base leading-6 tracking-[0%] font-poppins text-[#808080]'>Address Book</Link><br />
+            <Link className='font-normal text-base leading-6 tracking-[0%] font-poppins text-[#808080]'>My Payment Options</Link>
+        </div>
+        <p className='font-medium text-base leading-6 tracking-[0%] font-poppins mt-6'>My Orders</p>
+        <div className='w-[165px] h-[56px] ml-[35px] mt-4'>
+            <Link className='font-normal text-base leading-6 tracking-[0%] font-poppins text-[#808080]'>My Returns</Link><br />
+            <Link className='font-normal text-base leading-6 tracking-[0%] font-poppins text-[#808080]'>My Cancellations</Link>
+        </div>
+        <p className='font-medium text-base leading-6 tracking-[0%] font-poppins mt-4'>My WishList</p>
       </div>
     </div>
   )
