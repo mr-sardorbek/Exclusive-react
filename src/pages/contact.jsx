10c7765853ae87d49c896cc0b3003ce1adfdc1d4
mr-sardor <rozmatovsardor67@gmail.com>
2025-09-28 19:55:10 +0500

    contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Phone from '../assets/icons/phone.svg'
 import Mail from '../assets/icons/mail.svg'
+import { Button } from 'antd'
 
 const Contact = () => {
   return (
@@ -15,7 +16,8 @@ const Contact = () => {
         </p>
       </div>
 
-      <div className='px-[35px] pt-[40px] pb-[51px] w-[340px] h-[457px] opacity-100 rounded mb-[140px] mt-[80px]'>
+     <div className='flex items-center gap-[30px] mb-[140px] mt-[80px]'>
+         <div className='px-[35px] pt-[40px] pb-[51px] w-[340px] h-[457px] opacity-100 rounded shadow-[0px_1px_13px_0px_#0000000D]'>
         <div className='flex items-center gap-4'>
             <img src={Phone} alt="phone" />
             <p className=' font-medium text-base leading-6 tracking-[0%] font-poppins'>Call To Us</p>
@@ -34,6 +36,29 @@ const Contact = () => {
         <p className='font-normal text-sm leading-[21px] tracking-[0%] font-poppins mt-4'>Emails: support@exclusive.com</p>
 
       </div>
+
+      <div className='w-[800px] h-[457px] opacity-100 rounded shadow-[0px_1px_13px_0px_#0000000D] py-[40px] px-[31.5px]'>
+        <div className='flex items-center justify-between'>
+            <input className=' bg-[#F5F5F5] w-[235px] h-[50px] opacity-100 rounded outline-none py-[13px] pl-4
+        font-normal text-sm leading-[21px] tracking-[0%] font-poppins' placeholder='Your Name *' />
+
+         <input className=' bg-[#F5F5F5] w-[235px] h-[50px] opacity-100 rounded outline-none py-[13px] pl-4
+        font-normal text-sm leading-[21px] tracking-[0%] font-poppins' placeholder='Your Email *' />
+
+        <input className=' bg-[#F5F5F5] w-[235px] h-[50px] opacity-100 rounded outline-none py-[13px] pl-4
+        font-normal text-sm leading-[21px] tracking-[0%] font-poppins' placeholder='Your Phone *' />        
+
+        </div>
+
+        <textarea typeof='textarea' className=' bg-[#F5F5F5] w-[737px] h-[207px] opacity-100 rounded py-[13px] pl-4
+        font-normal text-sm leading-[21px] tracking-[0%] font-poppins mt-8 border-none outline-none resize-none' placeholder='Your Phone *' ></textarea>
+
+        <div className='flex justify-end mt-8'>
+            <Button type='primary'>Send Massage</Button>
+        </div>
+
+      </div>
+     </div>
     </div>
   )
 }
